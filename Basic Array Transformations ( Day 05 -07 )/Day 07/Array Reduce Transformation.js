879172ed9f1074a771ea7b394ecf68de08aee19c
securi3ytalent <devmehedi101@gmail.com>
2025-09-18 18:45:50 +0600

    Javascript Array Reduce Transformation Day 07

--- Basic Array Transformations ( Day 05 -07 )/Day 07/Array Reduce Transformation.js
+++ Basic Array Transformations ( Day 05 -07 )/Day 07/Array Reduce Transformation.js
@@ -1,12 +1,57 @@
+// 2626. Array Reduce Transformation 
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+
+//  Classic for loop
+
+var reduce = function(nums, fn, init) {
+    let total = init
+    for(i = 0; i<nums.length; i++){
+        total = fn(total, nums[i])
+    }
+    return total;
+
+};
 
 
+// for...of with index
+
+// var reduce = function(nums, fn, init) {
+//   let total = init;
+//   for (let [i, val] of nums.entries()) {
+//     total = fn(total, val, i); 
+//   }
+//   return total;
+// };
 
 
+// forEach
+
+// var reduce = function(nums, fn, init) {
+//   let total = init;
+//   nums.forEach((val, i) => {
+//     total = fn(total, val, i);
+//   });
+//   return total;
+// };
+
+// recursion
+
+// var reduce = function(nums, fn, init, i = 0) {
+//   if (i >= nums.length) return init;
+//   return reduce(nums, fn, fn(init, nums[i], i), i + 1);
+// };
 
 
+// built-in Array.prototype.reduce (meta-solution)
 
-
-
-
+// var reduce = function(nums, fn, init) {
+//   return nums.reduce((acc, val, i) => fn(acc, val, i), init);
+// };
 
 
