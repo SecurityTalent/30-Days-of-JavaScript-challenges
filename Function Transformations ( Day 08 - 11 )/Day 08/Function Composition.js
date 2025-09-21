320df97c75258c16721b58b26730d7d512d14cf3
securi3ytalent <devmehedi101@gmail.com>
2025-09-21 16:03:06 +0600

    Javascript Function Composition Day 08

---
+++ Function Transformations ( Day 08 - 11 )/Day 08/Function Composition.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var colutation = function(functions) {
+  return function(x) {
+    let result = x;
+    for (let i = 0; i < functions.length; i++) {
+      result = functions[i](result);
+    }
+    return result;
+  };
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
