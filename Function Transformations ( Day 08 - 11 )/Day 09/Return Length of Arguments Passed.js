b20bb349fd0d3d379e6b515aff5822a85ee7d360
securi3ytalent <devmehedi101@gmail.com>
2025-09-21 16:14:19 +0600

    Javascript 2703. Return Length of Arguments Passed Day 09

---
+++ Function Transformations ( Day 08 - 11 )/Day 09/Return Length of Arguments Passed.js
@@ -0,0 +1,8 @@
+/**
+ * Return the count of arguments passed to it.
+ * @param {*} args - A variable number of arguments.
+ * @returns {number} The count of arguments passed to the function.
+ */
+function argumentsLength(...args) {
+  return args.length;
+}
